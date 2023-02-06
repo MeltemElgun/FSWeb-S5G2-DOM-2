3b55a08c74c5698e01e656ef3c1673c885a0104d
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-02-06 12:32:15 +0300

    Dom-2.teslim

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
-
+document.querySelector("h1").addEventListener("click", function (e) {
+  alert("Bana tıkladın!");
+});
 
 // Kodlar buraya gelecek!
+document.querySelectorAll(".nav-link").forEach((item) => {
+  item.addEventListener("mouseover", (item) => {
+    item.target.style.color = "red";
+  });
+});
+
+document.querySelectorAll(".destination h4").forEach((item) => {
+  item.addEventListener(
+    "click",
+    (item) => (item.target.textContent = "merhaba!")
+  );
+});
+
+let imgOtobus = document.querySelector(".intro img");
+imgOtobus.addEventListener("select", grilestir);
+function grilestir() {
+  imgOtobus.setAttribute("style", "filter: grayscale(100%");
+}
+
+document.querySelectorAll(".btn").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    event.target.textContent = event.target.nodeName;
+  });
+});
